5d46ea82209eec637b06f71764ced89a73077743
Tiffany <tiff.couce@gmail.com>
2023-03-01 17:44:16 +0100

    Affichage des recettes par catégorie

--- cuisine/recettes/models.py
+++ cuisine/recettes/models.py
@@ -6,12 +6,14 @@ from django.utils import timezone
 class Categorie(models.Model):
     nom = models.CharField(max_length=20)
     modifie_le = models.DateTimeField(auto_now=True)
+    tri = models.IntegerField(unique=True, null=True)
     def __str__(self):
         return self.nom
 
 class Recette(models.Model):
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
     titre = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='media/recettes', null=True)
     modifie_le = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.titre
--- cuisine/recettes/views.py
+++ cuisine/recettes/views.py
@@ -7,8 +7,17 @@ class Categories(generic.ListView):
     model = Categorie
     template_name = 'recettes/categories.html'
 
+    def get_queryset(self):
+        return Categorie.objects.order_by('tri')
+
 class RecettesByCategorie(generic.ListView):
     template_name = 'recettes/recettes_by_categorie.html'
 
     def get_queryset(self):
         return Recette.objects.filter(categorie__id=self.kwargs['categorie_id'])
+
+    def get_context_data(self,**kwargs):
+        context = super(RecettesByCategorie,self).get_context_data(**kwargs)
+        context['categorie_list'] = Categorie.objects.order_by('tri')
+        context['categorie_id'] = self.kwargs['categorie_id']
+        return context
