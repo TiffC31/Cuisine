09937d1e7a9136d799b96ad7d098f0f2e80643d8
TiffC <tiff.couce@gmail.com>
2023-03-07 09:09:34 +0100

    url avec slug + active menu

--- cuisine/recettes/urls.py
+++ cuisine/recettes/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 app_name = "recettes"
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('<int:categorie_id>/', views.Liste.as_view(), name='recettes_by_categorie'),
+    path('categorie/<slug:slug>/', views.Liste.as_view(), name='recettes_by_categorie'),
     path('nouvelle/', views.RecetteCreateView.as_view(), name='nouvelle'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
