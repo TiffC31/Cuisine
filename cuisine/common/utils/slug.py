d7fb467a8aafc032275420e9bf2dd95c0974fef0
TiffC <tiff.couce@gmail.com>
2023-03-07 14:50:24 +0100

    Supp lignes debug

--- cuisine/common/utils/slug.py
+++ cuisine/common/utils/slug.py
@@ -4,9 +4,6 @@ def slug_unique(chaine, model, id):
     slug = slugify(chaine)
     unique_slug = slug
     nb = 2
-    f = open("debug/debug.txt", "w")
-    f.write(str(model.filter(slug=unique_slug).exists()))
-    f.close()
     while model.filter(slug=unique_slug).exists():
         if(model.filter(slug=unique_slug).values('id')[0]['id'] == id):
             break
